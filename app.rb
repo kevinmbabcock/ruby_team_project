74ce4ea57d628dba4512ebe1d45426456a43cec1
Malgorzata Haniszewska <gosia.haniszewska@gmail.com>
2017-12-18 15:25:00 -0800

    add tag_edit and attraction_edit to views

--- app.rb
+++ app.rb
@@ -31,6 +31,66 @@ patch ("/tag_form") do
   end
 end
 
-# get '/tags/1/edit' do
-#   "Hello World"
-# end
+patch ('/attraction_form') do
+  name = params[:name]
+  description = params[:description]
+  price = params[:price]
+  season = params[:seasons]
+  tag_ids = params[:tag_ids]
+  @attraction = Attraction.create({:name => name, :description => description, :price => price, :season => season, :tag_ids => tag_ids, :id => nil})
+  if @attraction.save()
+    @attractions = Attraction.all()
+    @tags = Tag.all()
+    erb(:attraction_form)
+  else
+    erb(:error)
+  end
+end
+
+get ('/tags/:id/edit') do
+  @tag = Tag.find(params[:id].to_i)
+  erb(:tag_edit)
+end
+
+delete('/tags/:id/edit') do
+  @tag = Tag.find(params[:id].to_i)
+  @tag.delete()
+  @tags = Tag.all()
+  erb(:tag_form)
+end
+
+patch('/tags/:id/edit') do
+  name = params[:name]
+  @tag = Tag.find(params[:id].to_i)
+  @tag.update({:name => name})
+  @tags = Tag.all()
+  erb(:tag_form)
+end
+
+get ('/attractions/:id/edit') do
+  @attraction = Attraction.find(params[:id].to_i)
+  @tags = Tag.all()
+  erb(:attraction_edit)
+end
+
+delete('/attractions/:id/edit') do
+  @attraction = Attraction.find(params[:id].to_i)
+  @attraction.delete()
+  @attractions = Attraction.all()
+  @tags = Tag.all()
+  erb(:attraction_form)
+end
+
+
+patch('/attractions/:id/edit') do
+  name = params[:name]
+  description = params[:description]
+  price = params[:price]
+  season = params[:seasons]
+  tag_ids = params[:tag_ids]
+  @attraction = Attraction.find(params[:id].to_i)
+  @attraction.update({:name => name, :description => description, :price => price, :season => season, :tag_ids => tag_ids})
+  @attractions = Attraction.all()
+  @tags = Tag.all()
+  erb(:attraction_form)
+end
