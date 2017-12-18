5514376a4a099a54b7f89578211c079b7e599a32
Malgorzata Haniszewska <gosia.haniszewska@gmail.com>
2017-12-18 11:26:08 -0800

    add tag_form .erb and attraction_form.erb to views

--- app.rb
+++ app.rb
@@ -4,7 +4,18 @@ Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get ("/") do
-  @tags = Tag.all()
   @attractions = Attraction.all()
+  @tags = Tag.all()
   erb(:index)
+end
+
+get ("/tag_form") do
+  @tags = Tag.all()
+  erb(:tag_form)
+end
+
+get ("/attraction_form") do
+  @attractions = Attraction.all()
+  @tags = Tag.all()
+  erb(:attraction_form)
 end
