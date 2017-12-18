6c79f31c4810ab49a88ede0f06c91a7f189d9e97
Malgorzata Haniszewska <gosia.haniszewska@gmail.com>
2017-12-18 10:29:49 -0800

    add index.erb view

--- app.rb
+++ app.rb
@@ -2,3 +2,9 @@ require("bundler/setup")
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get ("/") do
+  @tags = Tag.all()
+  @attractions = Attraction.all()
+  erb(:index)
+end
