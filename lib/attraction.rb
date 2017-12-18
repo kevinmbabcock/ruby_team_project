c10f5af8dbd30d0ed7ce86ab28222c79dcc384b2
Kevin Babcock <kevinmbabcock@icloud.com>
2017-12-18 10:50:36 -0800

    add validation for presence of name for both Attraction and Tag

--- lib/attraction.rb
+++ lib/attraction.rb
@@ -1,4 +1,5 @@
 class Attraction < ActiveRecord::Base
+  validates(:name, :presence => true)
   has_many :attraction_tags
   has_many :tags, through: :attraction_tags
 end
--- spec/attraction_spec.rb
+++ spec/attraction_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 
-describe (Attraction) do
-  it ('has many tags') do
+describe 'Attraction' do
+  it 'has many tags' do
     test_tag1 = Tag.create({:name => "tag1"})
     test_tag2 = Tag.create({:name => "tag2"})
     test_attraction = Attraction.create({:name => "Space Needle", :description => "none", :price => nil, :season => "Spring", :tag_ids => [test_tag1.id, test_tag2.id]})
     expect(test_attraction.tags).to(eq([test_tag1, test_tag2]))
+  end
+
+  it 'validates the presence of a name' do
+    attraction = Attraction.new({:name => ""})
+    expect(attraction.save).to(eq(false))
   end
 end
