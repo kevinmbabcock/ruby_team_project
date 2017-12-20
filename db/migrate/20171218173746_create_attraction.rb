a9ca6edcfc9cf6c4f903bc0958e5f86bea1e13e1
Kevin Babcock <kevinmbabcock@icloud.com>
2017-12-20 09:58:30 -0800

    change data type of price in database and fix test for Attraction#custom_update method

--- db/migrate/20171218173746_create_attraction.rb
+++ db/migrate/20171218173746_create_attraction.rb
@@ -4,7 +4,7 @@ class CreateAttraction < ActiveRecord::Migration[5.1]
       t.column(:name, :string)
       t.column(:description, :string)
       t.column(:season, :string, array: true, default: [])
-      t.column(:price, :money)
+      t.column(:price, :numeric)
       t.timestamps
     end
   end
